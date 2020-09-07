573fc87f99379e7f6f6fbaaa54dfd9d63323281f
Sam Jackson <smahstan@gmail.com>
2020-09-06 20:33:45 -0500

    Trying linear structure instead

--- src/node.hpp
+++ src/node.hpp
@@ -2,33 +2,61 @@
 #define NODE_HPP
 #define SUBDIVS 8
 
+typedef unsigned int LocCode;
+
 class Node {
-private:
-	class Branch {
-	public:
-		Node* leaf[SUBDIVS];
-
-		~Branch();
-	};
-
-	Branch* branch;
-	float width; //Spatial width of the node
-	float center[3]; //Location of the node
-
 public:
-	Node* parent;
-	short value; //Used to determine vertex location
-	void* data; //Optional aditional data
+	LocCode locCode;
 
-	Node (Node* parent, int value, float width, float* center);
+	Node (LocCode locCode);
 	virtual ~Node ();
 
-	Node* &operator[](int i); //Used to access child nodes
-	void subdivide(); //Subdivides the node into 8 child nodes
-	void collapse(); //Removes all child nodes
-	Node* root(); //Gets the root of the node structure
-	int level(); //Returns the nodes level in the node structure
+	LocCode parent();
 
 };
+
+template <class T>
+class Voxel : public Node {
+public:
+	float value;
+	T data;
+
+};
+
+class Vertex : public Node {
+public:
+	float location[3];
+	float normal[3];
+
+};
+
+// class Node {
+// private:
+// 	class Branch {
+// 	public:
+// 		Node* leaf[SUBDIVS];
+//
+// 		~Branch();
+// 	};
+//
+// 	Branch* branch;
+// 	float width; //Spatial width of the node
+// 	float center[3]; //Location of the node
+//
+// public:
+// 	Node* parent;
+// 	double value; //Used to determine vertex location
+// 	void* data; //Optional aditional data
+//
+// 	Node (Node* parent, int value, float width, float* center);
+// 	virtual ~Node ();
+//
+// 	Node* &operator()(int c...); //Used to access child nodes
+// 	void subdivide(); //Subdivides the node into 8 child nodes
+// 	void collapse(); //Removes all child nodes
+// 	Node* root(); //Gets the root of the node structure
+// 	int level(); //Returns the nodes level in the node structure
+//
+// };
 
 #endif
