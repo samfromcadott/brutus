fd487d28c315f85614b0a52147b98f9c7089ab18
samfromcadott <samahstan@gmail.com>
2023-06-22 11:48:24 -0500

    Added deconstructor for `Mesh`

--- include/brutus/brutus.h
+++ include/brutus/brutus.h
@@ -15,9 +15,16 @@ struct Mesh {
 	float* normals = nullptr;
 	float* tex_coords = nullptr;
 
-	unsigned short *indices = nullptr;
+	unsigned short* indices = nullptr;
 
 	size_t vertex_count = 0, face_count = 0;
+
+	~Mesh() {
+		if (vertices != nullptr) delete[] vertices;
+		if (normals != nullptr) delete[] normals;
+		if (tex_coords != nullptr) delete[] tex_coords;
+		if (indices != nullptr) delete[] indices;
+	}
 };
 
 #include "voxel.h"
