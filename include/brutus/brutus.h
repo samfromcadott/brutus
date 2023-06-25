d26e84675b91839d8aaa6d2dbef25181627907a2
samfromcadott <samahstan@gmail.com>
2023-06-25 08:51:30 -0500

    Moved `Mesh` to separate file

--- include/brutus/brutus.h
+++ include/brutus/brutus.h
@@ -9,24 +9,9 @@ namespace Brutus {
 class Grid;
 class Chunk;
 struct Voxel;
+struct Mesh;
 
-struct Mesh {
-	float* vertices = nullptr;
-	float* normals = nullptr;
-	float* tex_coords = nullptr;
-
-	unsigned short* indices = nullptr;
-
-	size_t vertex_count = 0, face_count = 0;
-
-	~Mesh() {
-		if (vertices != nullptr) delete[] vertices;
-		if (normals != nullptr) delete[] normals;
-		if (tex_coords != nullptr) delete[] tex_coords;
-		if (indices != nullptr) delete[] indices;
-	}
-};
-
+#include "mesh.h"
 #include "vectors.h"
 #include "voxel.h"
 #include "chunk.h"
