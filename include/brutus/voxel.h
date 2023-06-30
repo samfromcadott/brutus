8cdc20128430b7b6f63a31404c9ddf7fb3f185b6
samfromcadott <samahstan@gmail.com>
2023-06-30 12:53:04 -0500

    Fixed typos in comment.

--- include/brutus/voxel.h
+++ include/brutus/voxel.h
@@ -3,5 +3,5 @@ typedef uint8_t VoxelData;
 
 struct Voxel {
 	VoxelWeight weight = 0; ///< Used for mesh generation. Negative values are inside the mesh.
-	VoxelData data = 0; ///< Addition data for the voxel. Such as terrain type.
+	VoxelData data = 0; ///< Additional data for the voxel, such as terrain type.
 };
