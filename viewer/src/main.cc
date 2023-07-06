83f36656c060b0bbd52836542cf4b2c5de073fa3
samfromcadott <samahstan@gmail.com>
2023-07-06 11:12:39 -0500

    Created 3D view for viewer

--- viewer/src/main.cc
+++ viewer/src/main.cc
@@ -1,6 +1,41 @@
-#include <iostream>
+#include <raylib.h>
 
-int main(int argc, char const *argv[]) {
-	std::cout << "Brutus viewer" << '\n';
+int main(void) {
+	const int screenWidth = 800;
+	const int screenHeight = 450;
+
+	SetConfigFlags(FLAG_WINDOW_RESIZABLE);
+	InitWindow(screenWidth, screenHeight, "Brutus Viewer");
+
+	Camera3D camera = { 0 };
+	camera.position = (Vector3){ 0.0f, 10.0f, 10.0f };
+	camera.target = (Vector3){ 0.0f, 0.0f, 0.0f };
+	camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };
+	camera.fovy = 45.0f;
+	camera.projection = CAMERA_PERSPECTIVE;
+
+	Vector3 cubePosition = { 0.0f, 0.0f, 0.0f };
+
+	SetTargetFPS(60);
+
+	while ( !WindowShouldClose() ) {
+		BeginDrawing();
+
+			ClearBackground({32, 32, 32, 255});
+
+			BeginMode3D(camera);
+
+				DrawCube(cubePosition, 2.0f, 2.0f, 2.0f, BLUE);
+				DrawCubeWires(cubePosition, 2.0f, 2.0f, 2.0f, DARKBLUE);
+
+			EndMode3D();
+
+			DrawFPS(10, 10);
+
+		EndDrawing();
+	}
+
+	CloseWindow();
+
 	return 0;
 }
