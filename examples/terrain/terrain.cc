c7617ba6b89db6e7e4034a588fc31a8894469f44
samfromcadott <samahstan@gmail.com>
2024-03-02 10:26:21 -0600

    Pregenerate meshes befor rendering

--- examples/terrain/terrain.cc
+++ examples/terrain/terrain.cc
@@ -35,6 +35,15 @@ int main() {
 		}
 	}
 
+	// Save the meshes
+	std::vector< Brutus::Mesh > meshes;
+
+	for (size_t x = 0; x < grid.chunk_size().x - 1; x++)
+	for (size_t y = 0; y < grid.chunk_size().y - 1; y++)
+	for (size_t z = 0; z < grid.chunk_size().z - 1; z++) {
+		meshes.push_back(grid.generate_mesh(x, y, z));
+	}
+
 	std::cout << "Generated terrain" << '\n';
 
 	const int screenWidth = 1280;
@@ -55,18 +64,12 @@ int main() {
 			ClearBackground({32, 32, 32, 255});
 
 			BeginMode3D(camera.camera);
-				for (size_t x = 0; x < grid.chunk_size().x - 1; x++)
-				for (size_t y = 0; y < grid.chunk_size().y - 1; y++)
-				for (size_t z = 0; z < grid.chunk_size().z - 1; z++) {
-					Brutus::Mesh mesh = grid.generate_mesh(x, y, z);
-					render_mesh( mesh);
+				for (auto& m : meshes) {
+					render_mesh(m);
 				}
-
-				// Draw the bounds of the grid
-				const Color clear_grey = {224, 224, 224, 128};
-				Vector3 bounds = {(float)grid.total_size().x-1, (float)grid.total_size().y-1, (float)grid.total_size().z-1};
-				DrawCubeWiresV( Vector3Scale(bounds, 0.5), bounds, clear_grey );
 			EndMode3D();
+
+			DrawFPS(20, 20);
 
 		EndDrawing();
 	}
