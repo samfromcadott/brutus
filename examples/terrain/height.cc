d9d98326ae43c91f4a05c5289856c4041f9b034a
samfromcadott <samahstan@gmail.com>
2024-03-03 09:34:58 -0600

    Added cave generation

--- examples/terrain/height.cc
+++ examples/terrain/height.cc
@@ -4,14 +4,14 @@
 using namespace OpenSimplexNoise;
 
 std::vector<float> heightmap(size_t size_x, size_t size_y, size_t size_z) {
-	Noise noise;
+	Noise noise(1138);
 	std::vector<float> map;
 	map.resize(size_x * size_y);
 
 	for (size_t x = 0; x < size_x; x++)
 	for (size_t y = 0; y < size_y; y++) {
-		float sx = static_cast<float>(x) / static_cast<float>(size_x);
-		float sy = static_cast<float>(y) / static_cast<float>(size_y);
+		float sx = static_cast<float>(x) / static_cast<float>(size_x) * 4.0;
+		float sy = static_cast<float>(y) / static_cast<float>(size_y) * 4.0;
 
 		float height = noise.eval(sx, sy) * size_z;
 		height = std::abs(height);
--- examples/terrain/terrain.cc
+++ examples/terrain/terrain.cc
@@ -13,11 +13,8 @@
 
 using namespace OpenSimplexNoise;
 
-int main() {
-	std::cout << "Brutus terrain test" << '\n';
-	Brutus::Grid grid(16, 16, 4); // A large grid
-
-	auto map = heightmap(grid.total_size().x, grid.total_size().y, grid.total_size().z);
+void generate_terrain(Brutus::Grid& grid) {
+	auto map = heightmap(grid.total_size().x, grid.total_size().y, grid.total_size().z - 4);
 
 	// Generate terrain using the noise
 	for (size_t x = 0; x < grid.total_size().x; x++)
@@ -27,15 +24,39 @@ int main() {
 
 		for (size_t z = 0; z < grid.total_size().z; z++) {
 			// Find the difference between z and the height
-			int diff = (z - height) * 64;
+			int diff = (z - height) * 127;
 
 			// Set the weight
 			Brutus::VoxelWeight weight = std::clamp(diff, -128, 127);
 			grid(x, y, z).weight = weight;
 		}
 	}
+}
 
-	// Save the meshes
+void generate_caves(Brutus::Grid& grid) {
+	Noise noise(1138);
+
+	for (int x = 0; x < grid.total_size().x; x++)
+	for (int y = 0; y < grid.total_size().y; y++)
+	for (int z = 0; z < grid.total_size().z; z++) {
+		float sx = static_cast<float>(x) / static_cast<float>(grid.total_size().x) * 8.0;
+		float sy = static_cast<float>(y) / static_cast<float>(grid.total_size().y) * 8.0;
+		float sz = static_cast<float>(z) / static_cast<float>(grid.total_size().z) * 4.0;
+
+		float w = noise.eval(sx, sy, sz);
+		w += 0.5;
+		w *= 127;
+
+
+		int weight = std::clamp(w, -128.0f, 127.0f);
+		int current_weight = grid(x, y, z).weight;
+		weight = std::max(-weight, current_weight);
+
+		grid(x, y, z).weight = weight;
+	}
+}
+
+std::vector< Brutus::Mesh > generate_meshes(Brutus::Grid& grid) {
 	std::vector< Brutus::Mesh > meshes;
 
 	for (size_t x = 0; x < grid.chunk_size().x - 1; x++)
@@ -44,17 +65,27 @@ int main() {
 		meshes.push_back(grid.generate_mesh(x, y, z));
 	}
 
-	std::cout << "Generated terrain" << '\n';
+	return meshes;
+}
+
+int main() {
+	Brutus::Grid grid(16, 16, 4); // A large grid
+
+	generate_terrain(grid);
+	generate_caves(grid);
+	std::vector< Brutus::Mesh > meshes = generate_meshes(grid);
 
 	const int screenWidth = 1280;
 	const int screenHeight = 720;
 
 	SetConfigFlags(FLAG_WINDOW_RESIZABLE);
-	InitWindow(screenWidth, screenHeight, "Brutus Viewer");
+	InitWindow(screenWidth, screenHeight, "Terrain");
 
 	FlyCamera camera;
 
 	SetTargetFPS(60);
+
+	Noise noise;
 
 	while ( !WindowShouldClose() ) {
 		camera.update();
